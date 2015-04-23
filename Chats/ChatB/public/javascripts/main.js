b253d6cf31b044bb079d3a6489a9fd309ebd0a34
Fobbos08 <fobbos08@mail.ru>
2015-04-23 21:41:11 +0200

    bot fix

--- Chats/ChatB/public/javascripts/main.js
+++ Chats/ChatB/public/javascripts/main.js
@@ -1,15 +1,39 @@
 /**
  * Created by Эмиль on 20.04.2015.
  */
-setInterval(send, 200);
+var timeout = 200;
+var length = 7;
+var l=7;
+setTimeout(send, timeout);
+setInterval(updateValues, 500);
+
+function updateValues(){
+    if ($("#time")[0].value != "")
+    {
+        if ($("#time")[0].value > 0)
+        {
+            timeout = parseInt($("#time")[0].value);
+        }
+    }
+    if ($("#messageLength")[0].value != "")
+    {
+        if ($("#messageLength")[0].value > 0)
+        {
+            length = $("#messageLength")[0].value;
+        }
+    }
+    l = parseInt(length);
+    l+=2;
+}
 
 function send(){
+    setTimeout(send, timeout);
     $.ajax({
         url:'http://localhost:3000/sendMessage',
         method:'POST',
         data: {
-        login:Math.random().toString(36).substring(7),
-        message:Math.random().toString(36).substring(7)
+        login:Math.random().toString(36).substring(2,l),
+        message:Math.random().toString(36).substring(2,l)
         },
         success: function(data){
         },
@@ -17,4 +41,5 @@ function send(){
             alert('error');
         }
     });
+    return;
 }
